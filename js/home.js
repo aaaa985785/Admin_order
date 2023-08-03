d4c34f4a2a9c4bdfd8b3e0866cc03b0e32fc11c2
aaaa985785 <aaaa985785@gmail.com>
2023-08-03 23:10:38 +0800

    first commit

---
+++ js/home.js
@@ -0,0 +1,135 @@
+var ctx = document.getElementById("infoChart").getContext("2d");
+var myChart = new Chart(ctx, {
+  type: "line",
+  data: {
+    labels: [
+      "6 June",
+      "7 June",
+      "8 June",
+      "9 June",
+      "10 June",
+      "11 June",
+      "12 June",
+      "13 June",
+    ],
+    datasets: [
+      {
+        label: "Total Cost",
+        data: [900, 900, 2600, 2800, 2400, 800, 1000, 2000],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0)",
+          "rgba(54, 162, 235, 0)",
+          "rgba(255, 206, 86, 0)",
+          "rgba(75, 192, 192, 0)",
+          "rgba(153, 102, 255, 0)",
+          "rgba(255, 159, 64, 0)",
+          "rgba(255, 159, 64, 0)",
+          "rgba(255, 159, 64, 0)",
+        ],
+        borderColor: [
+          "rgba(255,99,132,1)",
+          "rgba(255,99,132,1)",
+          "rgba(255,99,132,1)",
+          "rgba(255,99,132,1)",
+          "rgba(255,99,132,1)",
+          "rgba(255,99,132,1)",
+          "rgba(255,99,132,1)",
+          "rgba(255,99,132,1)",
+        ],
+        pointBackgroundColor: [
+          "rgba(255,99,132,1)",
+          "rgba(255,99,132,1)",
+          "rgba(255,99,132,1)",
+          "rgba(255,99,132,1)",
+          "rgba(255,99,132,1)",
+          "rgba(255,99,132,1)",
+          "rgba(255,99,132,1)",
+          "rgba(255,99,132,1)",
+        ],
+      },
+      {
+        label: "Net Income",
+        data: [6000, 5000, 2000, 4000, 3800, 5000, 6200, 5000],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0)",
+          "rgba(54, 162, 235, 0)",
+          "rgba(255, 206, 86, 0)",
+          "rgba(75, 192, 192, 0)",
+          "rgba(153, 102, 255, 0)",
+          "rgba(255, 159, 64, 0)",
+        ],
+        borderColor: [
+          "rgba(74, 144, 226, 1)",
+          "rgba(74, 144, 226, 1)",
+          "rgba(74, 144, 226, 1)",
+          "rgba(74, 144, 226, 1)",
+          "rgba(74, 144, 226, 1)",
+          "rgba(74, 144, 226, 1)",
+          "rgba(74, 144, 226, 1)",
+          "rgba(74, 144, 226, 1)",
+        ],
+        pointBackgroundColor: [
+          "rgba(74, 144, 226, 1)",
+          "rgba(74, 144, 226, 1)",
+          "rgba(74, 144, 226, 1)",
+          "rgba(74, 144, 226, 1)",
+          "rgba(74, 144, 226, 1)",
+          "rgba(74, 144, 226, 1)",
+          "rgba(74, 144, 226, 1)",
+          "rgba(74, 144, 226, 1)",
+        ],
+      },
+      {
+        label: "Total Revenue",
+        data: [7500, 7000, 5200, 7000, 5800, 6100, 7800, 7700],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0)",
+          "rgba(54, 162, 235, 0)",
+          "rgba(255, 206, 86, 0)",
+          "rgba(75, 192, 192, 0)",
+          "rgba(153, 102, 255, 0)",
+          "rgba(255, 159, 64, 0)",
+        ],
+        borderColor: [
+          "rgba(126, 211, 33, 1)",
+          "rgba(126, 211, 33, 1)",
+          "rgba(126, 211, 33, 1)",
+          "rgba(126, 211, 33, 1)",
+          "rgba(126, 211, 33, 1)",
+          "rgba(126, 211, 33, 1)",
+          "rgba(126, 211, 33, 1)",
+          "rgba(126, 211, 33, 1)",
+        ],
+        pointBackgroundColor: [
+          "rgba(126, 211, 33, 1)",
+          "rgba(126, 211, 33, 1)",
+          "rgba(126, 211, 33, 1)",
+          "rgba(126, 211, 33, 1)",
+          "rgba(126, 211, 33, 1)",
+          "rgba(126, 211, 33, 1)",
+          "rgba(126, 211, 33, 1)",
+          "rgba(126, 211, 33, 1)",
+        ],
+      },
+    ],
+  },
+  options: {
+    elements: {
+      line: {
+        tension: 0, // disables bezier curves
+      },
+    },
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+            stepSize: 2000,
+          },
+        },
+      ],
+    },
+    responsive: true,
+    maintainAspectRatio: false,
+  },
+});
